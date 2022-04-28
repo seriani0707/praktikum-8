370bd7eaf6e541083d1ec598e7ea2d36142c10bc
seani0707 <seriyani774@gmail.com>
2022-04-28 06:46:01 -0700

    Praktikum 8

---
+++ blog/application/models/Mahasiswa_model.php
@@ -0,0 +1,18 @@
+<?php
+
+// Buat Class
+class Mahasiswa_model extends CI_Model{
+    // Buat Struktur Data
+    public $id, $nama, $nim, $gender, $tmp_lahir, $tgl_lahir, $ipk;
+
+    // Buat Method
+    public function predikat(){
+        // (Kodisi) ? True : false
+        $perdikat = ($this->ipk >= 3.75) ? "Cumlaude" : "Bagus";
+        return $predikat;
+
+        }
+    }
+
+
+    ?>
---
+++ blog/application/models/Matakuliah_model.php
@@ -0,0 +1,12 @@
+<?php
+
+// Buat Class
+class Matakuliah_model extends CI_Model{
+    // Buat Struktur Data
+    public $nama, $sks, $kode;
+
+   
+    }
+
+
+    ?>
